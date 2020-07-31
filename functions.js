7ad4078158c6865a6a8f518b1f8572939f80486c
Axel <bogosaxel@gmail.com>
2020-07-31 16:06:57 -0400

    Added search by category on Job portal

--- functions.js
+++ functions.js
@@ -16,5 +16,27 @@ function searchById() {
                 tr[i].style.display = "none";
             }
         }
+    }
 }
+
+function searchByCategory() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("category").valueOf();
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableBody");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        console.log(tr)
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
--- user_job_portal.php
+++ user_job_portal.php
@@ -1,20 +1,37 @@
 <?php
-//require 'config.php';TODO UNCOMMENT
+require 'config.php';
 session_start();
 ?>
 <HTML>
 <HEAD>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <script src="functions.js"></script>
+    <script src="functions.js">
+    </script>
 </HEAD>
 
 <BODY>
 <?php require 'user_dashboard_navbar.php' //nav bar
 ?>
 <br>
-<button onclick="searchById()">Search job by ID </button>
-<input type="text" id="jobID">
+<div>
+    <button onclick="searchById()">Search job by ID </button>
+    <input type="text" id="jobID">
+</div>
+<br>
+<br>
+<button onclick="searchByCategory()">Search job by category </button>
+<select id="category" size="1">
+    <option value=''hidden>Choose Category</option>
+    <?php
+    $sql = "SELECT category FROM 1Job";
+    $result = mysqli_query($db,$sql);
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<option value='".$row['category']."'>".$row['category']."</option>";
+    }
+    ?>
+</select>
+<br>
 <br>
 <div align="center">
     <table class="blueTable">
@@ -22,58 +39,33 @@ session_start();
         <tr>
             <th>ID</th>
             <th>Title</th>
+            <th>Description</th>
             <th>Category</th>
             <th>Post Date</th>
         </tr>
         </thead>
         <tfoot>
         <tr>
-            <td colspan="4">
+            <td colspan="5">
                 <div class="links"><a href="#">&laquo;</a> <a class="active" href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a> <a href="#">&raquo;</a></div>
             </td>
         </tr>
         </tfoot>
         <tbody id="tableBody">
-        <tr>
-            <td>0</td>
-            <td>Software Engineer</td>
-            <td>Software</td>
-            <td>01-01-2020</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Part-time Dictator</td>
-            <td>Politics</td>
-            <td>01-01-2020</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Cat Wrestler</td>
-            <td>Home</td>
-            <td>01-01-2020</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Couch Tester</td>
-            <td>Home</td>
-            <td>01-01-2020</td>
-        </tr>
         <?php
-        //TODO UNCOMMENT
-        /*
-        $sql = "SELECT jobID,title,briefDescription,postDate FROM 1Job";
+        $sql = "SELECT jobID,title,briefDescription,postDate,category FROM 1Job";
         $result = mysqli_query($db,$sql);
         while ($row = mysqli_fetch_array($result)) {
-        echo "<tr>";
+            echo "<tr>";
             echo "<td>".$row['jobID']."</td>";
             echo "<td>".$row['title']."</td>";
             echo "<td>".$row['briefDescription']."</td>";
+            echo "<td>".$row['category']."</td>";
             echo "<td>".$row['postDate']."</td>";
             echo "</tr>";
         }
-        */
         ?>
-
+        </tr>
         </tbody>
 
     </table>
