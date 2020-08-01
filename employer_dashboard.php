0f6e426faf47e92916a01486690635a47ead0737
Axel <bogosaxel@gmail.com>
2020-08-01 16:51:02 -0400

    initialized employer dashboard
    +employer navbar

---
+++ employer_dashboard.php
@@ -0,0 +1,26 @@
+<?php
+//require 'config.php'; //TODO UNCOMMENT
+session_start();
+?>
+<HTML>
+<HEAD>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+</HEAD>
+
+<BODY>
+<?php require 'employer_dashboard_navbar.php' //nav bar
+?>
+<div align="center">
+    <h2>
+        <br>
+        <?php
+        $accountID = $_SESSION['accountID'];
+        echo 'Hello '. $accountID ."! Please select an option above";
+        ?>
+    </h2>
+
+</div>
+</BODY>
+</HTML>
