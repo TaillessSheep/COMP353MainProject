1f14436447f66c10b31dfa8066a949c5acf38e19
Axel <bogosaxel@gmail.com>
2020-08-01 14:39:43 -0400

    Fix Search by category

--- functions.js
+++ functions.js
@@ -30,7 +30,7 @@ function searchByCategory() {
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
         console.log(tr)
-        td = tr[i].getElementsByTagName("td")[3];
+        td = tr[i].getElementsByTagName("td")[4];
         if (td) {
             txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
