2849746447bfee03d3b00e881826bf1f76e6dbe8
Axel <bogosaxel@gmail.com>
2020-08-06 19:00:10 -0400

    Add admin navigation bar
    Some links are not yet defined

---
+++ admin_dashboard_navbar.php
@@ -0,0 +1,22 @@
+<div class="navbar" style="margin: 0">
+    <a href="admin_dashboard.php"><i class="material-icons" style="color: white">home</i></a>
+    <a href="admin_job_portal.php">Job Portal</a>
+    <a href="admin_reports.php">User Reports</a>
+    <div class="dropdown">
+        <button class="dropbtn">Modify Profile
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="admin_update_profile.php">Update Profile</a>
+            <a href="delete_profile.php">Delete Profile</a>
+        </div>
+    </div>
+    <div class="topnav-right">
+        <a href="logout.php">Log out</a>
+        <a style="pointer-events: none;"> <?php
+            date_default_timezone_set('America/Toronto');
+            echo(date('d/m/Y', time()));?>
+        </a>
+    </div>
+</div>
+
