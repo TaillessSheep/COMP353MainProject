b127d12cc4dc363bca92fd76a4e4d45d3993f416
Axel <bogosaxel@gmail.com>
2020-08-08 09:36:13 -0400

    fix

--- employer_dashboard_navbar.php
+++ employer_dashboard_navbar.php
@@ -1,11 +1,14 @@
-<?php /* This file purpose is to be included at the top of the of the <BODY> of other files in order to display the navigation bar.
-        Necessary style sheet to be included in <HEAD>: <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-       */
+<?php
+require 'config.php';
+session_start();
+$sql = "SELECT status  FROM `1User` WHERE accountID = '".$_SESSION['accountID']."'";
+$result = mysqli_query($db,$sql);
+$row = mysqli_fetch_array($result);
+if($row['status']=='frozen')
+{
 ?>
 <div class="navbar" style="margin: 0">
     <a href="employer_dashboard.php"><i class="material-icons" style="color: white">home</i></a>
-    <a href="employer_job_portal.php">Search Jobs</a>
-    <a href="employer_published_jobs.php">My Published Jobs</a>
     <div class="dropdown">
         <button class="dropbtn">Modify Profile
             <i class="fa fa-caret-down"></i>
@@ -24,3 +27,35 @@
         </a>
     </div>
 </div>
+    <?php
+}
+else
+{
+    ?>
+    <div class="navbar" style="margin: 0">
+        <a href="employer_dashboard.php"><i class="material-icons" style="color: white">home</i></a>
+        <a href="employer_job_portal.php">Search Jobs</a>
+        <a href="employer_published_jobs.php">My Published Jobs</a>
+        <div class="dropdown">
+            <button class="dropbtn">Modify Profile
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="employer_modify_profile.php">Update Profile</a>
+                <a href="delete_profile.php">Delete Profile</a>
+            </div>
+        </div>
+        <a href="method_of_payment.php">Payment</a>
+        <div class="topnav-right">
+            <a href="logout.php">Log out</a>
+            <a style="pointer-events: none;"> <?php
+                date_default_timezone_set('America/Toronto');
+                echo(date('d/m/Y', time()));?>
+            </a>
+        </div>
+    </div>
+
+    <?php
+}
+?>
+
--- user_dashboard_navbar.php
+++ user_dashboard_navbar.php
@@ -1,24 +1,65 @@
-<div class="navbar" style="margin: 0">
-    <a href="user_dashboard.php"><i class="material-icons" style="color: white">home</i></a>
-    <a href="user_job_portal.php">Search Jobs</a>
-    <a href="user_applied_jobs.php">My Applications</a>
-    <div class="dropdown">
-        <button class="dropbtn">Modify Profile
-            <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-            <a href="user_modify_profile.php">Update Profile</a>
-            <a href="delete_profile.php">Delete Profile</a>
+<?php
+require 'config.php';
+session_start();
+$sql = "SELECT status  FROM `1User` WHERE accountID = '".$_SESSION['accountID']."';";
+$result = mysqli_query($db,$sql);
+$row = mysqli_fetch_array($result);
+if($row['status']=='frozen')
+{
+    ?>
+
+    <div class="navbar" style="margin: 0">
+        <div class="dropdown">
+            <button class="dropbtn">Modify Profile
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="user_modify_profile.php">Update Profile</a>
+                <a href="delete_profile.php">Delete Profile</a>
+            </div>
+        </div>
+        <a href="method_of_payment.php">Payment</a>
+        <div class="topnav-right">
+            <a href="logout.php">Log out</a>
+            <a style="pointer-events: none;">
+                <?php
+                date_default_timezone_set('America/Toronto');
+                echo(date('d/m/Y', time()));?>
+            </a>
         </div>
     </div>
-    <a href="method_of_payment.php">Payment</a>
-    <div class="topnav-right">
-        <a href="logout.php">Log out</a>
-        <a style="pointer-events: none;"> <?php
-            date_default_timezone_set('America/Toronto');
-            echo(date('d/m/Y', time()));?>
-        </a>
+    <?php
+}
+else
+{
+    ?>
+
+    <div class="navbar" style="margin: 0">
+        <a href="user_dashboard.php"><i class="material-icons" style="color: white">home</i></a>
+        <a href="user_job_portal.php">Search Jobs</a>
+        <a href=\"user_applied_jobs.php">My Applications</a>
+        <div class="dropdown">
+            <button class="dropbtn">Modify Profile
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="user_modify_profile.php">Update Profile</a>
+                <a href="delete_profile.php">Delete Profile</a>
+            </div>
+        </div>
+        <a href=\"method_of_payment.php">Payment</a>
+        <div class="topnav-right">
+            <a href="logout.php">Log out</a>
+            <a style="pointer-events: none;"> <?php
+                date_default_timezone_set('America/Toronto');
+                echo(date('d/m/Y', time()));?>
+            </a>
+        </div>
     </div>
-</div>
+    <?php
+}
+?>
+
+
 
 
